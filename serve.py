de6349d4df778b5bdc57e159a37244542eec46c3
Eduardo Alvarez <57263404+eduand-alvarez@users.noreply.github.com>
2024-08-19 21:46:13 -0500

    Update serve.py

--- serve.py
+++ serve.py
@@ -26,16 +26,6 @@ warnings.filterwarnings("ignore")
 logger.info("loading Intel/neural-chat-7b-v1-1")
 neural_chat_7B_v1_1_Model, neural_chat_7B_v1_1_Tokenizer = ITREXLoader('Intel/neural-chat-7b-v1-1')
 
-# neural_chat_7B_v3_1
-#logger.info("loading Intel/neural-chat-7b-v3-1")
-#neural_chat_7B_v3_1_streamer_queue = Queue()
-#neural_chat_7B_v3_1_Model, neural_chat_7B_v3_1_Tokenizer = ITREXLoader('Intel/neural-chat-7b-v3-1')
-
-#neural_chat_7B_v3_0_Model, neural_chat_7B_v3_0_Tokenizer = ITREXLoader('Intel/neural-chat-7b-v3')
-#neural_chat_7B_v3_2_Model, neural_chat_7B_v3_2_Tokenizer = ITREXLoader('Intel/neural-chat-7b-v3-2')
-
-
-
 # Generation initiator and response server
 async def response_generator(query, model, tokenizer, streamer, streamer_queue):
 
@@ -75,12 +65,6 @@ async def stream(payload:InferencePayload):
         return StreamingResponse(response_generator(payload.query, neural_chat_7B_v1_1_Model, 
                                                     neural_chat_7B_v1_1_Tokenizer, neural_chat_7B_v1_1_Streamer, 
                                                     neural_chat_7B_v1_1_streamer_queue), media_type='text/event-stream')
-    #elif model == 'Intel/neural-chat-7b-v3-1':
-    #    logger.info("Intel/neural-chat-7b-v3-1 selected for inference")
-    #    neural_chat_7B_v3_1_Streamer = CustomStreamer(neural_chat_7B_v3_1_streamer_queue, neural_chat_7B_v3_1_Tokenizer, True)
-    #    return StreamingResponse(response_generator(payload.query, neural_chat_7B_v3_1_Model, 
-    #                                                neural_chat_7B_v3_1_Tokenizer, neural_chat_7B_v3_1_Streamer, 
-    #                                                neural_chat_7B_v3_1_streamer_queue), media_type='text/event-stream')
 
 if __name__ == "__main__":
     uvicorn.run("serve:app", host="0.0.0.0", port=8080, log_level="info")
